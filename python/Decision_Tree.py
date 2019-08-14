2a04aa098a82d66f82cca982271ef24d17003258
NongDingAn <nda_1992@163.com>
2019-08-14 08:14:30 +0800

    更新了md文件

---
+++ python/Decision_Tree.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+# ~/anaconda3/bin/python3.6
+'''
+Author:NDA
+E-mail:nongdingan@126.com
+github:https://github.com/nda1992/Reading_Books_Notebook
+'''
+
+
+#####决策树的构建#####
+'''
+概括为3个步骤：特征选择、决策树的生成和决策树的修剪。
+
+-  特征选择在于选取对训练数据具有分类能力的特征
+
+
+'''
